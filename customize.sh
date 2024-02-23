20fa2234d55a1ef8187cb7c63c8064cf838ac280
Jin_Wang <Jin_Wang@vfc.com>
2024-02-23 18:48:18 +0800

    update customize.sh

--- customize.sh
+++ customize.sh
@@ -7,4 +7,5 @@
 #=================================================
 #1. Modify default IP
 sed -i 's/192.168.1.1/192.168.2.3/g' openwrt/package/base-files/files/bin/config_generate
+#2. Modify default gateway & DNS
 sed -i "/set network.\$1.netmask='\$netm'/a \\\t\t\t\tset network.\$1.gateway='192.168.2.2'\n\t\t\t\tset network.\$1.dns='192.168.2.2'" openwrt/package/base-files/files/bin/config_generate
